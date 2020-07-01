a035efde9df681fce7e1f32c99f9bff0e8a06c39
kwamkert <tomriddle477@gmail.com>
2020-07-01 08:54:45 +0000

    auth test complete

---
+++ src/utils/auth.js
@@ -0,0 +1,76 @@
+const expressJwt = require('express-jwt');
+const {omit} = require('lodash');
+const crypto = require('crypto')
+const secret = 'task-secret' 
+
+
+
+const dayInSeconds = 60 * 24 * 60
+
+
+function isPasswordAllowed(password){
+    return (
+        //password length
+        password.length > 6 &&
+        //nonalphanumeric
+        /\W/.test(password) &&
+        //digit 
+        /\d/.test(password) &&
+        //Capital letter
+        /[A-Z]/.test(password) &&
+        //lower letter
+        /[a-z]/.test(password) 
+    )
+}
+
+const authMiddleware = expressJwt({secret:'secret', algorithms: ['RS256']})
+
+
+function getSaltAndHash(password) {
+  const salt = crypto.randomBytes(16).toString('hex')
+  const hash = crypto
+    .pbkdf2Sync(password, salt, iterations, 512, 'sha512')
+    .toString('hex')
+  return {salt, hash}
+}
+
+
+function isPasswordValid(password, {salt, hash}) {
+  return (
+    hash ===
+    crypto.pbkdf2Sync(password, salt, iterations, 512, 'sha512').toString('hex')
+  )
+}
+    
+
+
+function  userToJSON(user){
+    //delete user.password;
+    
+    return omit(user, ['password']) ;
+
+}
+
+
+function getUserToken({id, username}) {
+  const issuedAt = Math.floor(now() / 1000)
+  return jwt.sign(
+    {
+      id,
+      username,
+      iat: issuedAt,
+      exp: issuedAt + dayInSeconds,
+    },
+    secret,
+  )
+}
+
+
+module.exports = {
+    isPasswordAllowed,
+    authMiddleware,
+    userToJSON,
+    getUserToken,
+    isPasswordValid,
+    getSaltAndHash
+}
---
+++ tests/utils/auth.test.js
@@ -0,0 +1,33 @@
+
+const { isPasswordAllowed, userToJSON }  = require('../../src/utils/auth');
+
+
+
+
+
+test("isPasswordValid returns true for valid password", ()=>{
+    expect(isPasswordAllowed("3!Aspass")).toBeTruthy();
+})
+
+
+test("isPasswordAllowed returns false for invalid password",()=>{
+
+    expect(isPasswordAllowed("password")).toBeFalsy();
+    expect(isPasswordAllowed("1234passd")).toBeFalsy();
+    expect(isPasswordAllowed("!!!sdf")).toBeFalsy();
+
+})
+
+
+test("user object doesnt have password ", ()=>{
+    
+    let user = {
+        username: 'kwame',
+        password: 'password'
+    }
+
+    let keys = Object.keys(userToJSON(user))
+    
+    expect(keys).not.toContain("password")
+
+})
