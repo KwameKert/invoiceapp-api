a4849fc893ecc8877864c6682f36385aafb54bb7
kwamkert <tomriddle477@gmail.com>
2020-07-07 15:47:06 +0000

    👷 login route test completed

--- src/routes/__tests__/auth.test.js
+++ src/routes/__tests__/auth.test.js
@@ -6,19 +6,31 @@ const  app = require('../../app');
 const request = require('supertest')
 
 const userId = new mongoose.Types.ObjectId();
-
+const url = "/api/auth"
 
 test("Create new user" , async()=>{
    
     const user = buildUser();
 
-    const response =    await request(app).get('/api/auth/test').send(user)
+    const response =    await request(app).post(`${url}/register`).send(user)
 
-     
    expect(response.statusCode).toEqual(201);
 })
 
 
+test("Login user", async() => {
 
+
+    const user = buildUser();
+    await request(app).post(`${url}/register`).send(user);
+
+    const response = await request(app)
+                            .post(`${url}/login`)
+                            .send({username: user.username,
+                                    password: user.password})
+
+    expect(response.statusCode).toEqual(202);
+
+})
 
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,11 @@
 const  express = require('express')
-
-//import AuthMiddleware from '../middlewares/auth'
-//import * as authController from '../controllers/auth.controller'
 const authController = require('../controllers/auth.controller')
 
-//function getAuthRoutes(){
-    
     const router = express.Router()
 
+    
     router.post('/register', authController.register);
-    router.get('/test', authController.testController); 
-  //  return router
-//}
+    router.post('/login', authController.login)   
+//router.get('/test', authController.testController); 
 
 module.exports = router
